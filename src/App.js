451226f4d6e638cedb123f7010c16c8010df6a36
dhruvit2003 <pateldhruvit2003@gmail.com>
2025-01-08 15:05:36 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Container>
+
+        </Container>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
